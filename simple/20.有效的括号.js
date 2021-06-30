4391bb4a04385480a47bebf175afbc242de1ccc8
anGaoLang <1774087845@qq.com>
2021-06-30 18:07:51 +0800

    久违的简单一题

---
+++ simple/20.有效的括号.js
@@ -0,0 +1,52 @@
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 示例 1： 输入：s = "()"      输出：true
+ * 示例 2： 输入：s = "()[]{}"  输出：true
+ * 示例 3： 输入：s = "(]"      输出：false
+ * 示例 4： 输入：s = "([)]"    输出：false
+ * 示例 5： 输入：s = "{[]}"    输出：true
+ * 
+ */
+
+/**
+ * 使用栈进行处理
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+  if (!s || s.length % 2) return false
+  const arr = s.split('')
+  const left = ['(', '[', '{']
+  const right = {
+      ')': '(',
+      ']': '[',
+      '}': '{'
+  }
+  const stack = []
+  let result = false
+  for (let i = 0; i < arr.length; i++) {
+      if (left.includes(arr[i])) {
+          stack.push(arr[i])
+          result = false
+      } else {
+          result = stack.pop() == right[arr[i]]
+          if (!result) return false
+      }
+  }
+  return stack.length == 0  && result
+};
+
+// ptython 直接成对的替换即可，最后为空便为真
+class Solution:
+  def isValid(self, s):
+      while '{}' in s or '()' in s or '[]' in s:
+          s = s.replace('{}', '')
+          s = s.replace('[]', '')
+          s = s.replace('()', '')
+      return s == ''
